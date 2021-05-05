62f91e2fadbd36f53c0ee750cca4714fa3994a1e
Joppe Aarts <joppe@apestaartje.info>
2021-05-05 11:20:27 +0200

    Added routing for home and login

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,26 @@
+<style lang="scss" global>
+</style>
+
 <script>
-    export let name;
+import router from 'page';
+import Home from './page/Home.svelte';
+import Login from './page/Login.svelte';
+
+let page;
+
+router('/', () => {
+    page = Home;
+});
+router('/login', () => {
+    page = Login;
+});
+
+router.start();
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <h1>test</h1>
+    <a href="/">Home</a>
+    <a href="/login">Login</a>
+    <svelte:component this="{page}" />
 </main>
-
-<style lang="scss" global>
-
-</style>
