fd24bbcd87c4ee9d0dfcf672416da2b2ae7aa44e
Joppe Aarts <joppe@apestaartje.info>
2021-05-05 14:03:49 +0200

    Table component added

---
+++ src/component/table/Table.svelte
@@ -0,0 +1,64 @@
+<style lang="scss">
+@import '../../style/config/_color.scss';
+@import '../../style/config/_spacing.scss';
+
+table {
+    border-spacing: 0;
+    width: 100%;
+
+    td,
+    th {
+        padding: map-get($spacing, 'md');
+    }
+
+    th {
+        text-align: left;
+        border-bottom: 1px solid $black;
+    }
+
+    tbody tr {
+        &.current {
+            background-color: $blue--maya;
+        }
+
+        &:nth-of-type(odd):not(.active) {
+            background-color: $grey--dark-gray;
+        }
+
+        &:hover:not(.disabled) {
+            background-color: $green--de-york;
+            cursor: pointer;
+        }
+    }
+}
+</style>
+
+<script>
+export let fields;
+export let data;
+export let styler;
+</script>
+
+<table>
+    <thead>
+        <tr>
+            {#each fields as field}
+                <th>{field.label}</th>
+            {/each}
+        </tr>
+    </thead>
+
+    <tbody>
+        {#each data as entry}
+            <tr class="{styler(entry)}">
+                {#each fields as field}
+                    <td>
+                        {field.mapper
+                            ? field.mapper(entry[field.prop])
+                            : entry[field.prop]}
+                    </td>
+                {/each}
+            </tr>
+        {/each}
+    </tbody>
+</table>
