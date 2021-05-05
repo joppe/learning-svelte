a88d0ce3e2d315df638390e7db0c9aa1ea71fe66
Joppe Aarts <joppe@apestaartje.info>
2021-05-05 14:04:27 +0200

    Added 404 page

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,52 @@
-<style lang="scss" global>
-@import './style/main.scss';
+<style lang="scss">
+:global {
+    @import './style/main.scss';
+}
+
+@import './style/config/_spacing.scss';
+nav {
+    ul {
+        display: flex;
+        justify-content: flex-end;
+    }
+
+    li {
+        position: relative;
+        padding: 0 map-get($spacing, 'sm');
+
+        &:before {
+            content: '|';
+            position: absolute;
+            left: 0;
+            top: 0;
+        }
+
+        &:first-of-type:before {
+            content: '';
+        }
+
+        &:last-of-type {
+            padding-right: 0;
+        }
+    }
+
+    button {
+        margin: 0;
+        padding: 0;
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+        text-decoration: underline;
+        line-height: 1.2em;
+    }
+}
 </style>
 
 <script>
 import router from 'page';
 import Home from './page/Home.svelte';
 import Login from './page/Login.svelte';
+import NotFound from './page/NotFound.svelte';
 import { isReady } from './firebase/firebase';
 import { isLoggedIn, logout } from './firebase/useAuth';
 import { user } from './store/user';
@@ -29,20 +70,32 @@ user.subscribe((v) => {
     router('/login', () => {
         page = Login;
     });
+    router('*', () => {
+        page = NotFound;
+    });
 
     router.start();
 })();
 </script>
 
-<main>
-    <h1>test</h1>
-    <a href="/">Home</a>
-    {#if loggedInUser}
-        <button type="button" on:click="{logout}">Logout</button>
-    {:else}
-        <a href="/login">Login</a>
-    {/if}
+<header class="o-container">
+    <nav>
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                {#if loggedInUser}
+                    <button type="button" on:click="{logout}">Logout</button>
+                {:else}
+                    <a href="/login">Login</a>
+                {/if}
+            </li>
+        </ul>
+    </nav>
+</header>
 
+<main>
     {#if page}
         <svelte:component this="{page}" />
     {/if}
