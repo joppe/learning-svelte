6a3d602453ab503f48b81b11496cd222826c9197
Joppe Aarts <joppe@apestaartje.info>
2021-05-05 13:04:36 +0200

    Use Form and FormField component in Login page

--- src/page/Login.svelte
+++ src/page/Login.svelte
@@ -1,6 +1,8 @@
 <script>
 import router from 'page';
 
+import Form from '../component/form/Form.svelte';
+import FormField from '../component/form/FormField.svelte';
 import { login } from '../firebase/useAuth';
 
 let username;
@@ -17,22 +19,26 @@ async function handleLogin() {
 
 <h1>Login</h1>
 
-<form>
-    <div>
-        <label for="form-field-username">Username</label>
-        <input
-            type="text"
-            id="form-field-username"
-            autocomplete="username"
-            bind:value="{username}" />
-    </div>
-    <div>
-        <label for="form-field-password">Password</label>
-        <input
-            type="password"
-            id="form-field-password"
-            autocomplete="current-password"
-            bind:value="{password}" />
-    </div>
-    <button type="button" on:click="{handleLogin}">Login</button>
-</form>
+<Form>
+    <svelte:fragment slot="fields">
+        <FormField id="form-field-username" label="Username">
+            <input
+                type="text"
+                id="form-field-username"
+                autocomplete="username"
+                bind:value="{username}" />
+        </FormField>
+
+        <FormField id="form-field-password" label="Password">
+            <input
+                type="password"
+                id="form-field-password"
+                autocomplete="current-password"
+                bind:value="{password}" />
+        </FormField>
+    </svelte:fragment>
+
+    <svelte:fragment slot="actions">
+        <button type="button" on:click="{handleLogin}">Login</button>
+    </svelte:fragment>
+</Form>
