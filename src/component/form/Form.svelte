18c0b668ce79b3a38c1c30e4d15dfec33d95f5ee
Joppe Aarts <joppe@apestaartje.info>
2021-05-05 13:04:15 +0200

    Added Form and FormField component

---
+++ src/component/form/Form.svelte
@@ -0,0 +1,33 @@
+<style lang="scss">
+@import '../../style/config/_spacing.scss';
+
+form {
+    padding: map-get($spacing, 'md');
+
+    &.inline {
+        display: flex;
+        padding: 0;
+    }
+}
+
+.actions {
+    display: flex;
+    justify-content: flex-end;
+
+    form.inline & {
+        margin-top: auto;
+    }
+}
+</style>
+
+<script>
+export let isInline = false;
+</script>
+
+<form class:inline="{isInline}">
+    <slot name="fields" />
+
+    <div class="actions">
+        <slot name="actions" />
+    </div>
+</form>
---
+++ src/component/form/FormField.svelte
@@ -0,0 +1,43 @@
+<style lang="scss">
+@import '../../style/config/_color.scss';
+@import '../../style/config/_font-size.scss';
+@import '../../style/config/_spacing.scss';
+
+div {
+    &.row {
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 20px;
+
+        label {
+            margin-bottom: 10px;
+        }
+    }
+
+    &.column {
+        width: 150px;
+        margin-right: map-get($spacing, 'md');
+
+        &.small {
+            width: 50px;
+        }
+
+        label {
+            font-size: map-get($font-size, 'sm');
+            color: $grey--dark-gray;
+        }
+    }
+}
+</style>
+
+<script>
+export let id;
+export let label;
+export let isInline = false;
+export let isSmall = false;
+</script>
+
+<div class:column="{isInline}" class:row="{!isInline}" class:small="{isSmall}">
+    <label for="{id}">{label}</label>
+    <slot />
+</div>
